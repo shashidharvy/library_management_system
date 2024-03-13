278d6df01751961decff124fea005ee02315d2e3
shashidhar-dd <shashidhar@dentaldost.com>
2024-03-13 20:58:05 +0530

    additions: logging

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import logging
+
 import book
 import check
 import user
+
+logging.basicConfig(
+    filename='library_events.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
 
 class InputManager:
